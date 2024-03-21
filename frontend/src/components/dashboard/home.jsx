76c5b4180bc652cf5856056ab863d3378f08cf01
rohitagr0310 <rohitagr2610@gmail.com>
2024-03-21 23:00:07 +0530

    working table and chart: line

--- frontend/src/components/dashboard/home.jsx
+++ frontend/src/components/dashboard/home.jsx
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-const HomeTest = (props) => {
+const Home = (props) => {
     const { loggedIn, setLoggedIn } = props;
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [username, setUsername] = useState('');
     const [file, setFile] = useState();
     const [cells, setCells] = useState();
+    const [chartData, setChartData] = useState({});
+
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+    );
 
     useEffect(() => {
         // Check if user is logged in from local storage
@@ -39,14 +61,49 @@ const HomeTest = (props) => {
                 const text = event.target.result;
                 const rows = text.toString().split('\n')
                 const rowsWithCell = rows.map((row) => row.split(','))
-                setCells(rowsWithCell)
+                setCells(rowsWithCell);
+
+                // Process data for chart
+                const labels = rowsWithCell[0].slice(1); // Assuming first row is labels and first column is not x-axis
+                const datasets = [];
+                for (let i = 1; i < rowsWithCell.length; i++) {
+                    const data = rowsWithCell[i].slice(1).filter((item) => !Number.isNaN(parseFloat(item)))
+                    console.log(data)// Assuming data starts from the second column
+                    datasets.push({
+                        label: `Dataset${i}`, // Assuming first column is label for each dataset
+                        data: data,
+                        fill: false,
+                        borderColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.6)`,
+                    });
+                }
+                console.log({
+                    labels: labels,
+                    datasets: datasets,
+                })
+                setChartData({
+                    labels: labels,
+                    datasets: datasets,
+                });
             };
             fileReader.readAsText(file);
         }
     };
 
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart',
+            },
+        },
+    };
+
     return (
-        <Box className="mainContainer" sx={{ padding: '20px' }}>
+        <Box className="mainContainer" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
             <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
                 <div>Welcome!</div>
             </Box>
@@ -72,25 +129,30 @@ const HomeTest = (props) => {
                             </button>
                         </form>
                     </Box>
-                    <Box>
-                        <table>
-                            <thead>
-                                <tr>
-                                    {cells && Array.isArray(cells) && cells[0]?.map((item) => (
-                                        <th key={item}>{item}</th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {cells && Array.isArray(cells) && cells.map((row, index) => (
-                                    <tr key={row[0] + index}>
-                                        {row && row.map((item) => (
-                                            <td key={item}>{item}</td>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                        <Box sx={{ width: '48%', height: '500px', overflow: 'scroll' }}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        {cells && Array.isArray(cells) && cells[0]?.map((item) => (
+                                            <th key={item}>{item}</th>
                                         ))}
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {cells && Array.isArray(cells) && cells.map((row, index) => (
+                                        <tr key={row[0] + index}>
+                                            {row && row.map((item) => (
+                                                <td key={item}>{item}</td>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </Box>
+                        {chartData?.labels && <Box sx={{ width: '48%' }}>
+                            <Line options={options} data={chartData} />
+                        </Box>}
                     </Box>
                 </Box>
             ) : (
@@ -100,4 +162,4 @@ const HomeTest = (props) => {
     );
 }
 
-export default HomeTest;
+export default Home;
